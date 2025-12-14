4eefbbe876c2de6cef8e80c0763d030900474bac
KasiaNiewiadomska <kasia.niewiadomska0516@gmail.com>
2025-12-14 12:55:00 +0100

    tekst

--- Pigeon_Invaders/FormGameStart.cs
+++ Pigeon_Invaders/FormGameStart.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Windows.Forms;
 using WMPLib;
 
+
+//TO TEKST DODANY CZY WIDZISZ LUBIE ŻABY I RYBKI POZDRAWIAM
+
 namespace Pigeon_Invaders
 {
     public partial class FormGameStart : Form
